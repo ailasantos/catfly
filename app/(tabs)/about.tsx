d3d2cdf261a0ab174725622e6a5b729cde89d8f3
unknown <aila.santos@preve.com.br>
2025-08-12 14:58:12 -0300

    ajuste na página sobre

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -2,7 +2,18 @@ import { Text, View, StyleSheet } from "react-native";
 export default function AboutScreen() {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>A lua brilha com mistérios—venha desvendá-los.</Text>
+            <Text style={styles.text}>A Lua muda de forma porque vemos diferentes partes iluminadas pelo Sol.
+O ciclo tem quatro fases principais:{'\n'}
+
+Nova: invisível, tempo de recomeço.{'\n'}
+
+Crescente: a luz aumenta, simboliza crescimento.{'\n'}
+
+Cheia: totalmente iluminada, é plenitude.{'\n'}
+
+Minguante: a luz diminui, é tempo de recolhimento.{'\n'}
+
+Um ciclo eterno de luz e sombra, como a própria vida..</Text>
             
         </View>
     );
@@ -16,9 +27,9 @@ const styles = StyleSheet.create({
     },
     text: {
         color: 'white',
-        fontSize: 17,
-        fontFamily: "Dancing Script"  
-
+        fontSize: 30,
+        fontFamily: "Dancing Script",  
+        textAlign: 'center',
     },
    
 });
