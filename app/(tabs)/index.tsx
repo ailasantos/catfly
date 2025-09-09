a8069f39cd23ca27964e79fbe0c1ffd982923768
unknown <aila.santos@preve.com.br>
2025-09-09 16:45:41 -0300

    Coloquei os emoji

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,29 @@
-import { Text, View, StyleSheet } from "react-native";
+import * as ImagePicker from 'expo-image-picker';
+import {useState} from "react";
+
+import { Text, View, StyleSheet, ImageSourcePropType } from "react-native";
 import ImageViewer from '@/components/ImageViewer';
 import { Image } from "expo-image";
 import Button from '@/components/Button';
-import * as ImagePicker from 'expo-image-picker';
-import {useState} from "react";
 import IconButton from "@/components/IconButton";
 import CircleButton from "@/components/CircleButton";
+import EmojiPicker from '@/components/EmojiPicker';
+import EmojiList from '@/components/EmojiList';
+import EmojiSticker from '@/components/EmojiSticker';
+import { GestureHandlerRootView} from 'react-native-gesture-handler';
+
+
 
 const PlaceholderImage = require('@/assets/images/luabrilhante.jpg');
 
 export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [isModalVisible,setIsModalVisible] = useState<boolean>(false);
+  const [pickedEmoji, setPickedEmoji]= useState<ImageSourcePropType | undefined>(undefined)
 
+  
+   
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ['images'],
@@ -34,6 +45,11 @@ export default function Index() {
   };
 
   const onAddSticker = () =>{
+    setIsModalVisible(true);
+  };
+
+    const onModalClose = () =>{
+    setIsModalVisible(false);
   };
 
   const onSaveImageAsync = async () => {
@@ -41,10 +57,11 @@ export default function Index() {
   };
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <Text style={styles.text}>A lua revela o que está além</Text>
         <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage}/>
+        {pickedEmoji && <EmojiSticker imageSize={40} stickerSource ={pickedEmoji} />}
       </View>
       {showAppOptions ? (
 
@@ -62,7 +79,10 @@ export default function Index() {
           <Button label="Use esta foto" onPress={() => setShowAppOptions(true)} />
         </View>
       )}
-    </View>
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickedEmoji} onCloseModal = {onModalClose}/>
+      </EmojiPicker>
+    </GestureHandlerRootView>
   );
 }
 const styles = StyleSheet.create({
